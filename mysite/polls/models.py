d4b156715c0468fcf7bc55c70967fbb72128321e
Arthur César Costa Sales <57773827+arthurgit1@users.noreply.github.com>
2020-12-17 22:46:36 -0300

    Update models.py

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -6,7 +6,7 @@ class Question(models.Model):
     # ...
     def was_published_recently(self):
         now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self: Question.pub_date <= now
+        return now - datetime.timedelta(days=1) <= Question
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
@@ -14,5 +14,5 @@ class Question(models.Model):
 class Choice(models.Model):
     # ...
     def __str__(self):
-        return self.choice_text
+        return
 
