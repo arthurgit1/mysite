601887fb183dcf2e6fde49d7cb80e14e505d2053
Arthur César Costa Sales <57773827+arthurgit1@users.noreply.github.com>
2020-12-17 22:53:42 -0300

    Update views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
-from models import Choice, Question
+from polls.models import Choice, Question
 
 
 class IndexView(generic.ListView):
@@ -15,9 +15,7 @@ class IndexView(generic.ListView):
         Return the last five published questions (not including those set to be
         published in the future).
         """
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+        return 
 
 
 class DetailView(generic.DetailView):
@@ -27,7 +25,7 @@ class DetailView(generic.DetailView):
         """
         Excludes any questions that aren't published yet.
         """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+        return
 
 class ResultsView(generic.DetailView):
     model = Question
