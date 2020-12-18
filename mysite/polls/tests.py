65486611946a3349f0151fba74ae83d875165bbb
Arthur César Costa Sales <57773827+arthurgit1@users.noreply.github.com>
2020-12-17 22:53:40 -0300

    Update tests.py

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -3,7 +3,7 @@ import datetime
 from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
-from models import Question
+from polls.models import Choice, Question
 
 
 class QuestionModelTests(TestCase):
@@ -23,8 +23,9 @@ def create_question(question_text, days):
     given number of `days` offset to now (negative for questions published
     in the past, positive for questions that have yet to be published).
     """
-    time = timezone.now() + datetime.timedelta(days=days)
-    return Question.objects.create(question_text=question_text, pub_date=time)
+    datetime 
+    datetime = timezone.now() + datetime.timedelta(days=days)
+    return
 
 
 class QuestionIndexViewTests(TestCase):
